6a8a1d3bf0535538daa31f2c6e93c153dcd1d0c7
iamzhz <zhzqqyd@qq.com>
2024-11-30 12:48:33 +0800

    change: Rich instead colorama

--- func/user.py
+++ func/user.py
@@ -1,5 +1,6 @@
 import get_api
-from colorama import Fore, Back, Style
+from rich.console import Console
+console = Console()
 def user_info(args):
     if len(args) == 0:
         print("Please enter the username.")
@@ -12,10 +13,10 @@ def user_info(args):
         print('User not found.')
         return
     # status == 200
-    print(f"{Fore.MAGENTA}Username{Style.RESET_ALL}: {info['username']}")
-    print(f"{Fore.MAGENTA}Nickname{Style.RESET_ALL}: {info['nickname']}")
-    print(f"{Fore.MAGENTA}Id{Style.RESET_ALL}: {info['id']}")
-    print(f"{Fore.MAGENTA}Sign{Style.RESET_ALL}: {info['sign']}")
+    console.print(f"[u]Username[/u]: {info['username']}")
+    console.print(f"[u]Nickname[/u]: {info['nickname']}")
+    console.print(f"[u]Id[/u]: {info['id']}")
+    console.print(f"[u]Sign[/u]: {info['sign']}")
 def user(args):
     if len(args) == 0:
         print("Please enter the args.")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from parse_line import *
 from call_other import *
-from colorama import Fore, Back, Style
+from rich.console import Console
+console = Console()
 def main():
     line = ''
     
     print('Welcome to Ban-Terminal!\nType "help" for help.')
     while True:
-        line = input(f'{Fore.GREEN}>{Style.RESET_ALL} ')
+        console.print('> ', style='green', end='')
+        line = input()
         line_result = parse_line(line)
         call_other(line_result[0], line_result[1])
 if __name__ == '__main__':
