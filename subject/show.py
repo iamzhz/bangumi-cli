1b4f86f73cf0310d0644432f7a8a3b04e9d8d351
iamzhz <zhzqqyd@qq.com>
2025-01-20 12:33:57 +0800

    feat(subject): show info

--- subject/show.py
+++ subject/show.py
@@ -2,6 +2,7 @@ from the_path import the_path
 from rich.panel import Panel
 from rich.columns import Columns
 from rich.console import Console
+from rich.table import Table
 console = Console()
 
 
@@ -12,10 +13,12 @@ def show(args):
     func_dict = {
         "summary": show_summary,
         "tags": show_tags,
+        "info": show_info,
     }
     if args[0] in func_dict:
         func_dict[args[0]]()
-    # it's no problem that a command is not found
+    else:
+        console.print(f"[red]TIP: [/red] {args[0]} is not a valid option !")
 
 
 def show_summary():
@@ -27,3 +30,16 @@ def show_tags():
     original_tags = the_path.path_des['more']['tags']
     tags = [f"{i.get('name')}: [green]{i.get('count')}[/green]" for i in original_tags]
     console.print(Columns(tags, equal=True))
+
+
+def show_info():
+    original_infobox = the_path.path_des['more']['infobox']
+    infobox = Table(title=the_path.path_des['name'])
+    infobox.add_column("Key", justify="right", style="cyan", no_wrap=True)
+    infobox.add_column("Value", style="magenta")
+    for i in original_infobox:
+        value = i['value']
+        if type(i['value']) is list:
+            value = "\n".join(v['v'] for v in i['value'])
+        infobox.add_row(i['key'], value)
+    console.print(infobox)
