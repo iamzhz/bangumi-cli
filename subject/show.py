984bdf8e3491e29c6f4677180c09b5898d6a41fa
iamzhz <zhzqqyd@qq.com>
2025-01-20 22:16:49 +0800

    feat(subject): show collection & id

--- subject/show.py
+++ subject/show.py
@@ -14,6 +14,8 @@ def show(args):
         "summary": show_summary,
         "tags": show_tags,
         "info": show_info,
+        "collection": show_collection,
+        "id": show_id,
     }
     if args[0] in func_dict:
         func_dict[args[0]]()
@@ -33,13 +35,27 @@ def show_tags():
 
 
 def show_info():
-    original_infobox = the_path.path_des['more']['infobox']
-    infobox = Table(title=the_path.path_des['name'])
+    original_infobox = the_path.path_des['more']['infobox']  # string
+    infobox = Table(title=the_path.path_des['name'])  # rich.table.Table
     infobox.add_column("Key", justify="right", style="cyan", no_wrap=True)
-    infobox.add_column("Value", style="magenta")
+    infobox.add_column("Value", style="white")
     for i in original_infobox:
         value = i['value']
         if type(i['value']) is list:
             value = "\n".join(v['v'] for v in i['value'])
         infobox.add_row(i['key'], value)
     console.print(infobox)
+
+
+def show_collection():
+    original_collection = the_path.path_des['more']['collection']  # dict
+    collection = Table(title=f"{the_path.path_des['name']} Collection")  # rich.table.Table
+    collection.add_column("Type", justify="right", style="cyan", no_wrap=True)
+    collection.add_column("Count", style="white")
+    for i in original_collection:
+        collection.add_row(str(i), str(original_collection[i]))
+    console.print(collection)
+
+
+def show_id():
+    console.print(f"ID: {the_path.path_des['id']}")
